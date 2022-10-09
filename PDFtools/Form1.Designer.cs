71ae9d67210cfc90fb70b47c23cef6de26304461
theangkko <theangkko@gmail.com>
2022-10-09 09:34:14 +0900

    first commit

---
+++ PDFtools/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace PDFtools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.radioBunpassword = new System.Windows.Forms.RadioButton();
+            this.radioBRemoveAtto = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label1.Location = new System.Drawing.Point(20, 64);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(411, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DRAG AND DROP HERE";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(1, 154);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "theangkko";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // radioBunpassword
+            // 
+            this.radioBunpassword.AutoSize = true;
+            this.radioBunpassword.Location = new System.Drawing.Point(6, 28);
+            this.radioBunpassword.Margin = new System.Windows.Forms.Padding(2);
+            this.radioBunpassword.Name = "radioBunpassword";
+            this.radioBunpassword.Size = new System.Drawing.Size(66, 16);
+            this.radioBunpassword.TabIndex = 2;
+            this.radioBunpassword.Text = "Decrypt";
+            this.radioBunpassword.UseVisualStyleBackColor = true;
+            this.radioBunpassword.CheckedChanged += new System.EventHandler(this.radioBunpassword_CheckedChanged);
+            // 
+            // radioBRemoveAtto
+            // 
+            this.radioBRemoveAtto.AutoSize = true;
+            this.radioBRemoveAtto.Checked = true;
+            this.radioBRemoveAtto.Location = new System.Drawing.Point(6, 6);
+            this.radioBRemoveAtto.Margin = new System.Windows.Forms.Padding(2);
+            this.radioBRemoveAtto.Name = "radioBRemoveAtto";
+            this.radioBRemoveAtto.Size = new System.Drawing.Size(132, 16);
+            this.radioBRemoveAtto.TabIndex = 3;
+            this.radioBRemoveAtto.TabStop = true;
+            this.radioBRemoveAtto.Text = "Remove Annotation";
+            this.radioBRemoveAtto.UseVisualStyleBackColor = true;
+            this.radioBRemoveAtto.CheckedChanged += new System.EventHandler(this.radioBRemoveAtto_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 173);
+            this.Controls.Add(this.radioBRemoveAtto);
+            this.Controls.Add(this.radioBunpassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDF Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.RadioButton radioBunpassword;
+        private System.Windows.Forms.RadioButton radioBRemoveAtto;
+    }
+}
+
---
+++ PDFtools/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFtools
+{
+    public partial class Form1 : Form
+    {
+        private string optionParameter = "annot remove ";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            this.Text = "PDFtools :: unpassword and remove annotations";
+            this.MaximizeBox = false;
+            this.AllowDrop = true;
+            this.DragDrop += (s, e) =>
+            {
+            };
+            this.DragEnter += (s, e) =>
+            {
+                var effects = DragDropEffects.None;
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    var path = ((string[])e.Data.GetData(DataFormats.FileDrop)).First();
+
+                    if (File.Exists(path))
+                    {
+                        var extension = Path.GetExtension(path).ToLower();
+                        if (extension == ".pdf")
+                        {
+
+                            effects = DragDropEffects.Move;
+                            var extIndex = path.LastIndexOf('.');
+                            var newfileName = path.Replace(path.Substring(extIndex, path.Length - extIndex), "_unpw.pdf");
+                            File.Copy(path, newfileName);
+                            //ExcelFileExtension.SaveAsCSV(path, newPDF);
+                            //Process.Start(newPDF);
+                            Process.Start("pdfcpu.exe", "annot remove " + newfileName);
+
+                        }
+                    }
+                }
+                e.Effect = effects;
+
+            };
+            this.DragLeave += (s, e) =>
+            {
+                Console.WriteLine("Leave");
+            };
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void radioBRemoveAtto_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioBRemoveAtto.Checked == true)
+            {
+                optionParameter = "annot remove ";
+            }
+            else
+            {
+                //optionParameter = "decrypt ";
+            }
+
+        }
+
+        private void radioBunpassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioBunpassword.Checked == true)
+            {
+                optionParameter = "decrypt ";
+            }
+            else
+            {
+                //optionParameter = "decrypt ";
+            }
+
+        }
+    }
+}
