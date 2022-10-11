5c1dfea6bc1a9a7f869094409f26f2650974965e
theangkko <theangkko@gmail.com>
2022-10-12 06:08:02 +0900

    .

--- PDFtools/Form1.Designer.cs
+++ PDFtools/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
@@ -42,7 +41,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(47, 95);
+            this.label1.Location = new System.Drawing.Point(51, 92);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(304, 48);
@@ -53,12 +52,12 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label2.Location = new System.Drawing.Point(325, 35);
+            this.label2.Location = new System.Drawing.Point(319, 39);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 18);
+            this.label2.Size = new System.Drawing.Size(70, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "theangkko";
             // 
@@ -100,27 +99,24 @@
             this.comboBoxSelectMode.Items.AddRange(new object[] {
             "GhostScript",
             "PDFcpu"});
-            this.comboBoxSelectMode.Location = new System.Drawing.Point(331, 6);
+            this.comboBoxSelectMode.Location = new System.Drawing.Point(262, 6);
             this.comboBoxSelectMode.Name = "comboBoxSelectMode";
             this.comboBoxSelectMode.Size = new System.Drawing.Size(127, 20);
             this.comboBoxSelectMode.TabIndex = 4;
-            this.comboBoxSelectMode.Text = "Select Method";
             this.comboBoxSelectMode.SelectedIndexChanged += new System.EventHandler(this.comboBoxSelectMode_SelectedIndexChanged);
-            this.comboBoxSelectMode.SelectedIndex = 0;
             // 
             // Form1
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(470, 173);
+            this.ClientSize = new System.Drawing.Size(401, 216);
             this.Controls.Add(this.comboBoxSelectMode);
             this.Controls.Add(this.radioBRemoveAtto);
             this.Controls.Add(this.radioBunpassword);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
